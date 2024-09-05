e4b57c32bef909c6a302b2bce58a4f277062835b
Lucas <90753591+lucasDS-0@users.noreply.github.com>
2024-09-04 22:02:19 -0300

    funciona un poco

--- taller_ipc/pipes/Ejercicio1/mini-shell.c
+++ taller_ipc/pipes/Ejercicio1/mini-shell.c
@@ -8,8 +8,8 @@
 
 typedef char*** matrix;
 
-void ejecutar_cmd(char* cmd, char* fila) {
-  execlp(cmd, fila, NULL);
+void ejecutar_cmd(char* cmd, char** fila) {
+  execvp(cmd, fila);
 }
 
 /*
@@ -31,50 +31,52 @@ void ejecutar_cmd(char* cmd, char* fila) {
 */
 
 void ejecutar_hijo_1(int count, matrix progs, int pipe_fd[][2]) {
-  //printf("tengo %d filas y en esta fila %d columnas\n", 
-  //            sizeof(pipe_fd)/sizeof(pipe_fd[0]),
-  //            sizeof(pipe_fd[0])/sizeof(pipe_fd[0][1]));
-  // Cerrar lectura
+  
   close(pipe_fd[0][PIPE_READ]);
+  // Conectar escritura a stdout
+  dup2(pipe_fd[0][PIPE_WRITE], STD_OUTPUT);
+  // Cerrar lectura
   for (int i = 1; i < count; i++) {
     close(pipe_fd[i][PIPE_READ]);
     close(pipe_fd[i][PIPE_WRITE]);
   }
-  // Conectar escritura a stdout
-  dup2(pipe_fd[0][PIPE_WRITE], STD_OUTPUT);
   // Ejecutar programa
   ejecutar_cmd(progs[0][0], progs[0]);
 }
         
 void ejecutar_hijo_i(int count, matrix progs, int i, int pipe_fd[][2]) {
 
-  for (int j = 1; j < count; j++) {
-    if (i != j) {
+  // Conectar lectura a stdin
+  dup2(pipe_fd[i-1][PIPE_READ], STD_INPUT);
+  // Conectar escritura a stdout
+  dup2(pipe_fd[i][PIPE_WRITE], STD_OUTPUT);
+  
+  for (int j = 0; j < count; j++) {
+    if (j == i - 1) {
+      close(pipe_fd[j][PIPE_WRITE]);
+    } else if (i == j) {   
+      close(pipe_fd[j][PIPE_READ]);
+    } else {
       close(pipe_fd[j][PIPE_READ]);
       close(pipe_fd[j][PIPE_WRITE]);
     }
   }
 
-  // Conectar lectura a stdin
-  dup2(pipe_fd[i][PIPE_READ], STD_INPUT);
-  // Conectar escritura a stdout
-  dup2(pipe_fd[i][PIPE_WRITE], STD_OUTPUT);
   // Ejecutar programa
   ejecutar_cmd(progs[i][0], progs[i]);
 } 
 
 void ejecutar_hijo_n(int count, matrix progs, int pipe_fd[][2]) {
-  // Cerrar escritura
-  // printf("estoy x cerrar en el ultimo hijo\n");
+  // Conectar lectura a stdin
   close(pipe_fd[count - 1][PIPE_WRITE]);
+  dup2(pipe_fd[count - 1][PIPE_READ], STD_INPUT);
+  // Cerrar escritura
   for (int j = 0; j < count-1; j++) {
       close(pipe_fd[j][PIPE_READ]);
       close(pipe_fd[j][PIPE_WRITE]);
   }
-  // Conectar lectura a stdin
-  dup2(pipe_fd[count - 1][PIPE_READ], STD_INPUT);
   // Ejecutar programa
-  ejecutar_cmd(progs[count - 1][0], progs[count - 1]);
+  ejecutar_cmd(progs[count][0], progs[count]);
 }
 
 void ejecutar_hijo(matrix progs, int i, int count, int pipe[][2]) {
@@ -91,15 +93,13 @@ static int run(matrix progs, size_t count)
 {	
 	int r, status;
 
-	//Reservo memoria para el arreglo de pids
-	//TODO: Guardar el PID de cada proceso hijo creado en children[i]
-	///////////////////////
 	
 	int pipes[count - 1][2];
 	for (int i = 0; i < count - 1; i++) { 
 	  pipe(pipes[i]);
 	}
 	
+	//Reservo memoria para el arreglo de pids
 	pid_t *children = malloc(sizeof(*children) * count);
 	
 	pid_t pid;
@@ -112,15 +112,14 @@ static int run(matrix progs, size_t count)
 	  }
 	}
 	
-		
+        //TODO: Para cada proceso hijo:
+	//1. Redireccionar los file descriptors adecuados al proceso
+	//2. Ejecutar el programa correspondiente
 
-	
-	//TODO: Pensar cuantos procesos necesito ///////////////       N
-	//TODO: Pensar cuantos pipes necesito. /////////////////       N - 1
-
-	//TODO: Para cada proceso hijo:
-	      //1. Redireccionar los file descriptors adecuados al proceso
-	      //2. Ejecutar el programa correspondiente
+        for (int i = 0; i < count - 1; i++) {  
+          close(pipes[i][PIPE_WRITE]);
+          close(pipes[i][PIPE_READ]);
+        }
 
 	//Espero a los hijos y verifico el estado que terminaron
 	for (int i = 0; i < count; i++) {
@@ -135,12 +134,6 @@ static int run(matrix progs, size_t count)
 	}
 	r = 0;
 	free(children);
-
-        close(pipes[count - 1][PIPE_WRITE]);
-        for (int i = 0; i < count - 1; i++) {  
-          close(pipes[i][PIPE_WRITE]);
-          close(pipes[i][PIPE_READ]);
-        }
 
 	return r;
 }
